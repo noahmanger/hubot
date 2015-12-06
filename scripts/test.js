9cf797ec6cfaf95e4ff1962952fea2fee1ebfb48
Noah Manger <noah.manger@gmail.com>
2015-12-05 18:06:23 -0800

    New scripts

--- scripts/test.js
+++ scripts/test.js
@@ -2,4 +2,8 @@ module.exports = function(robot) {
   robot.hear('/testing/i', function(res) {
     res.send('I see you');
   });
+
+  robot.respond('/hi buddy/', function(res) {
+    res.send('HELLLOOOOO!');
+  })
 }
